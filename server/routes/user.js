f95a9a39d38929ccbb0e911846413067306f8592
Rajan Jha <110784081+rajan-2003@users.noreply.github.com>
2024-08-10 00:23:28 +0530

    Add user authentication routes for sign-up and sign-in
    - Created routes for user sign-up and sign-in in the authentication router.
    - Applied input validation for sign-up, ensuring name, email, and password meet required criteria.
    - Linked routes to respective controllers: `signIn` for login and `signUp` for registration.

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const { body } = require("express-validator");
+const { signIn, signUp } = require("../controllers/userAuthController");
+
+const userValidation = [
+  body("name").notEmpty().withMessage("Name is required"),
+  body("email").isEmail().withMessage("Invalid email format"),
+  body("password")
+    .isLength({ min: 5 })
+    .withMessage("Password must be at least 5 characters long"),
+];
+
 // Sign-in route
 router.post("/signin", signIn);
 
 // Sign-up route
-router.post("/signup", signUp);
+router.post("/signup", userValidation, signUp);
 
 module.exports = router;
