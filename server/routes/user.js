2d3573ef5c4019ae23e289be5063569e9cc0419b
Rajan Jha <110784081+rajan-2003@users.noreply.github.com>
2024-08-11 23:05:17 +0530

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator");
-const { signIn, signUp } = require("../controllers/userAuthController");
+const {
+  signIn,
+  signUp,
+  updateProfile,
+  tokenIsValid,
+  getUser,
+} = require("../controllers/userAuthController");
 
 const userValidation = [
   body("name").notEmpty().withMessage("Name is required"),
@@ -11,10 +17,28 @@ const userValidation = [
     .withMessage("Password must be at least 5 characters long"),
 ];
 
+const auth = require("../middleware/auth");
+
 // Sign-in route
 router.post("/signin", signIn);
 
 // Sign-up route
 router.post("/signup", userValidation, signUp);
 
+// Update user route
+// router.post("/update", userValidation, updateProfile);//yeh removed hai iska code likhna hai
+// Sign-in route
+router.post("/signin", signIn);
+
+// Sign-up route
+router.post("/signup", userValidation, signUp);
+
+//tokenIs Valid route
+router.post("/tokenIsValid", tokenIsValid);
+
+//ise get request bna diya ha
+//get user data
+router.get("/getUser", getUser);
+
 module.exports = router;
+
