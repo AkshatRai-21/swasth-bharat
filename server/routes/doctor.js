b3e55796e4c17992cffd49bf996c1e2bcfa9b6ce
Rajan Jha <110784081+rajan-2003@users.noreply.github.com>
2024-08-11 21:09:27 +0530

    Update doctor.js

--- server/routes/doctor.js
+++ server/routes/doctor.js
@@ -4,6 +4,14 @@ const {
   getAllDoctor,
   speciality,
   nearestDoctor,
+  signInDoctor,
+  signUpDoctor,
+  tokenIsValidDoctor,
+  getDoctor,
+  searchDoctors,
+  saveDateTimeSlots,
+  removeDateTimeSlot,
+  availableDateTimeSlots,
 } = require("../controllers/doctorController");
 
 const {
@@ -12,16 +20,40 @@ const {
   logoutDoctor,
 } = require("../controllers/authController");
 
+const admin = require("../middleware/auth_doctor");
+
 const router = express.Router();
 
-//Authentication routes of Doctor
 router.route("/register").post(registerDoctor);
 router.route("/login").post(loginDoctor);
 router.route("/logout").post(logoutDoctor);
 
-//Query routes of the Doctor
 router.get("/all", getAllDoctor);
 router.get("/specialty", speciality);
 router.get("/near", nearestDoctor);
+router.get("/search-doctor", searchDoctors);
+
+// Sign-in route
+router.post("/signin", signInDoctor);
+
+// Sign-up route
+router.post("/signup", signUpDoctor);
+
+//tokenIs Valid route
+router.post("/tokenIsValid", tokenIsValidDoctor);
+
+//get user data
+router.post("/getDoctor", admin, getDoctor);
+
+//appointments docotr
+
+// Endpoint to save date and time slots
+router.post("/:doctorId/saveDateTimeSlots", saveDateTimeSlots);
+
+// Endpoint to fetch available date and time slots
+router.get("/:doctorId/availableDateTimeSlots", availableDateTimeSlots);
+
+// remove
+router.delete("/:doctorId/removeDateTimeSlot", removeDateTimeSlot);
 
 module.exports = router;
