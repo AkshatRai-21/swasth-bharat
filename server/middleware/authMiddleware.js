59b57ec672161186984df42d218de238cfbb743b
Rajan Jha <110784081+rajan-2003@users.noreply.github.com>
2024-08-11 16:14:28 +0530

    fix(auth): Correct JWT decoding and doctor assignment

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,3 +1,22 @@
-const authMiddleware = (req, res, next) => {};
+const jwt = require("jsonwebtoken");
+const Doctor = require("../models/doctor");
+// const User = require("../models/user");
 
-module.exports = authMiddleware;
+module.exports = function (req, res, next) {
+  const token = req.header("x-auth-token");
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET || "JCDKJCFKDSLFJSDAFJDLCFJDSLKFJDLKFLFDSAKF"
+    );
+
+    req.doctor = decoded.Doctor;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
